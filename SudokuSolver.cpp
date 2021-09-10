9bb6854a6fa9f0c39bfc61d39e7d0eaadc49743c
saran <sandysaluja7@gmail.com>
2021-09-10 18:53:33 +0530

    Diagonal check in box updated

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -39,7 +39,7 @@ bool solve(vector<vector <int>>&board)
                     return false;
                 if(board[row][i]==k)
                     return false;
-                if(board[3 * (row / 3) + i % 3][3 * (col/3) + i % 3]==k)
+                if(board[3 * (row / 3) + i / 3][3 * (col/3) + i % 3]==k)
                     return false;
             }
             return true;
