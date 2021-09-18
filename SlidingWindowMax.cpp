e49813931027ba3358004b44c1472746575bb80a
saran <sandysaluja7@gmail.com>
2021-09-18 13:23:01 +0530

    Sliding Window Maximum of size k less than x

--- SlidingWindowMax.cpp
+++ SlidingWindowMax.cpp
@@ -1,24 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-void slidingwindow(int arr[],int size, int k)
+void slidingwindow(int arr[],int n, int k, int x)
 {
-    deque<int> dq;
-    for(int i=0;i<size;i++)
+    // deque<int> dq;
+    // for(int i=0;i<size;i++)
+    // {
+    //     if(!dq.empty() && dq.front()==i-k)
+    //     {
+    //         dq.pop_front();
+    //     }
+    //     while(!dq.empty() && arr[dq.back()]<arr[i])
+    //     {
+    //         dq.pop_back();
+    //     }
+    //     dq.push_back(i);
+    //     if(i>=k-1)
+    //     {
+    //         cout<<arr[dq.front()]<<endl;
+    //     }
+    // }
+    int sum=0,ans=0;
+    for(int i=0;i<k;i++)
     {
-        if(!dq.empty() && dq.front()==i-k)
+        sum+=arr[i];
+    }
+    if(sum<x)
+    {
+        ans=sum;
+    }
+    for(int i=k;i<n;i++)
+    {
+        sum=sum-arr[i-k];
+        sum+=arr[i];
+
+        if(sum<x)
         {
-            dq.pop_front();
-        }
-        while(!dq.empty() && arr[dq.back()]<arr[i])
-        {
-            dq.pop_back();
-        }
-        dq.push_back(i);
-        if(i>=k-1)
-        {
-            cout<<arr[dq.front()]<<endl;
+            ans=max(ans,sum);
         }
     }
+    cout<<ans<<endl;
 }
 int main()
 {
@@ -31,6 +51,8 @@ int main()
     }
     int k;
     cin>>k;
-    slidingwindow(arr,n,k);
+    int x;
+    cin>>x;
+    slidingwindow(arr,n,k,x);
     return 0;
 }
